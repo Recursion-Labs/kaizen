152c47a58aea232f15f63bfe3ca70c8236de42a8
samarth3301 <samarplayz1337@gmail.com>
2025-10-19 17:16:35 +0530

    refactor: streamline background service initialization and remove unused code

--- chrome-extension/src/background/index.ts
+++ chrome-extension/src/background/index.ts
@@ -1,40 +1,36 @@
-import "webextension-polyfill";
-import { exampleThemeStorage } from "@extension/storage";
+import {
+  interventionScheduler,
+  metricsCollector,
+  behaviorMonitor,
+} from "./behaviour";
 
-exampleThemeStorage.get().then((theme) => {
-  console.log("theme", theme);
-});
-
-console.log("Background loaded");
-console.log(
-  "Edit 'chrome-extension/src/background/index.ts' and save to reload.",
-);
-
-// Open side panel when extension icon is clicked
-chrome.action.onClicked.addListener((tab) => {
-  if (tab.id) {
-    chrome.sidePanel.open({ tabId: tab.id });
-  }
-});
-
-// Add context menu to open popup as an option
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: "openPopup",
-    title: "Open Kaizen Popup",
-    contexts: ["action"],
-  });
-});
-
-// Handle context menu clicks
-chrome.contextMenus.onClicked.addListener((info) => {
-  if (info.menuItemId === "openPopup") {
-    // Open popup window
-    chrome.windows.create({
-      url: chrome.runtime.getURL("popup/index.html"),
-      type: "popup",
-      width: 400,
-      height: 600,
+const handleIntervention = (alarm: chrome.alarms.Alarm) => {
+  console.log("Executing intervention logic for:", alarm.name);
+  if (alarm.name === "limitExceeded") {
+    chrome.notifications.create({
+      type: "basic",
+      iconUrl: "icon128.png",
+      title: "Usage Limit Reached",
+      message: "You have visited this site frequently. Time for a break?",
     });
   }
-});
+};
+const scheduler = new interventionScheduler.InterventionScheduler(
+  handleIntervention,
+);
+const metrics = new metricsCollector.MetricsCollector(scheduler);
+const monitor = new behaviorMonitor.BehaviorMonitor(
+  () => {
+    /* Handle tab activation if needed */
+  },
+  (tabId: unknown, changeInfo: unknown, tab: chrome.tabs.Tab) =>
+    metrics.handleTabUpdate(tab),
+);
+
+console.log("Background service worker started and modules initialized.");
+
+export default {
+  scheduler,
+  metrics,
+  monitor,
+};
