e94bea514923abf4f8fd630083088875be900c4f
Ayush <ayushsarkar357@gmail.com>
2025-10-23 14:08:40 +0530

    refactor: updated indec.ts

--- chrome-extension/src/background/behaviour/index.ts
+++ chrome-extension/src/background/behaviour/index.ts
@@ -1,3 +1,4 @@
 export * as behaviorMonitor from "./behavior-monitor";
 export * as interventionScheduler from "./intervention-scheduler";
 export * as metricsCollector from "./metrics-collector";
+export { IntegrationManager } from "./integration-manager";
